75c7bf0732d03046f455a6b9d4a375624359f2f6
Sara Báez <sara.bzg8@gmail.com>
2014-05-29 16:36:09 +0100

    Merge

--- visor_cliente/main.cpp
+++ visor_cliente/main.cpp
@@ -6,12 +6,12 @@ typedef std::vector<cv::Mat> ImagesType;
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    ViewerWindow w;
 
     QCoreApplication::setOrganizationName("Videovigilancia");
     QCoreApplication::setOrganizationDomain("https://github.com/ull-etsii-sistemas-operativos/videovigilancia-1314-g07");
     QCoreApplication::setApplicationName("viewer");
 
+    ViewerWindow w;
     w.show();
 
     return a.exec();
--- visor_servidor/main.cpp
+++ visor_servidor/main.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    ViewerWindow w;
 
     QCoreApplication::setOrganizationName("Videovigilancia");
     QCoreApplication::setOrganizationDomain("https://github.com/ull-etsii-sistemas-operativos/videovigilancia-1314-g07");
     QCoreApplication::setApplicationName("viewer");
 
+    ViewerWindow w;
     w.show();
 
     return a.exec();
